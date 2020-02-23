3be356c0ef39af631843894294ba0dac28950f9c
Uttuess <458594033@qq.com>
2020-02-23 22:50:27 +0800

    首页部分组件+导航栏处理

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,7 +3,21 @@
     <nav-bar class="nav-bar">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
+    <home-swiper class="home-swiper" :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <fashion-view></fashion-view>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
   </div>
 </template>
   
@@ -11,6 +25,8 @@
 import NavBar from 'components/common/navBar/NavBar'
 
 import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+import FashionView from './childComps/FashionView'
 
 import {getHomeMultipleData,
         BANNER,
@@ -38,7 +54,9 @@ export default {
   },
   components:{
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView,
+    FashionView,
   },
 }
 </script>
@@ -47,5 +65,13 @@ export default {
 .nav-bar{
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+.home-swiper{
+  margin-top: 44px;
 }
 </style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="recommend-view">
+    <div class="recommend-item" v-for="item in recommends">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <span>{{item.title}}</span>
+      </a>
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+  props:{
+    recommends:{
+      type:Array,
+    }
+  },
+  beforeUpdate(){
+    console.log(this.recommends);
+  }
+}
+</script>
+  
+<style scoped>
+.recommend-view{
+  display: flex;
+  border-bottom: 10px solid #eeeeee;
+}
+.recommend-item{
+  text-align: center;
+  padding: 8px 2px 12px;
+}
+.recommend-item img{
+  width: 100%;
+}
+.recommend-item span{
+  font-size: 14px
+}
+</style>
